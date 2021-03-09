a05f0a04429f3bc7dbf190bb64b76d37d3ebeb55
Simon Jürgensmeyer <simon.juergensmeyer@gmail.com>
2021-03-10 00:00:17 +0100

    Fix args

--- main.go
+++ main.go
@@ -36,69 +36,84 @@ func main() {
 		log.Fatalln("need config file")
 	}
 
-	for _, configPath := range os.Args {
+	var configs []Config
+	for _, configPath := range os.Args[1:] {
+		log.Printf("parsing config %v\n", configPath)
+
 		configFile, err := os.ReadFile(configPath)
 		if err != nil {
 			log.Fatalf("failed reading config (%v): %v\n", "TODO confg file", err)
 		}
 
 		config := ConfibleFile{}
-		toml.Unmarshal(configFile, &config)
+		if err := toml.Unmarshal(configFile, &config); err != nil {
+			log.Printf("failed unmarshalling config file: %v\n", err)
+		}
 
-		for _, cfg := range config.Configs {
-			if strings.HasPrefix(cfg.Target, "~") {
-				home, err := os.UserHomeDir()
-				if err != nil {
-					log.Fatalf("failed getting home dir: %v\n", err)
-				}
-				cfg.Target = filepath.Join(home, cfg.Target[1:])
-			}
+		// Aggregate all configs before executing
+		configs = append(configs, config.Configs...)
 
-			targetFile, err := os.OpenFile(cfg.Target, os.O_CREATE, 0o666)
+		// TODO: run commands
+		// config.Commands
+	}
+
+	modifyFiles(configs)
+}
+
+func modifyFiles(configs []Config) {
+	for _, cfg := range configs {
+		if strings.HasPrefix(cfg.Target, "~") {
+			home, err := os.UserHomeDir()
 			if err != nil {
-				log.Fatalf("failed reading target file (%v): %v\n", cfg.Target, err)
+				log.Fatalf("failed getting home dir: %v\n", err)
 			}
-			defer targetFile.Close()
+			cfg.Target = filepath.Join(home, cfg.Target[1:])
+		}
 
-			newContent := strings.Builder{}
+		targetFile, err := os.OpenFile(cfg.Target, os.O_CREATE, 0o666)
+		if err != nil {
+			log.Fatalf("failed reading target file (%v): %v\n", cfg.Target, err)
+		}
+		defer targetFile.Close()
 
-			scanner := bufio.NewScanner(targetFile)
-			skip := false
-			for scanner.Scan() {
-				if strings.Contains(scanner.Text(), header) {
-					skip = true
-				}
+		newContent := strings.Builder{}
 
-				if strings.Contains(scanner.Text(), footer) {
-					skip = false
-					continue
-				}
-
-				if skip {
-					continue
-				}
-
-				newContent.Write(scanner.Bytes())
-				newContent.WriteByte('\n')
+		scanner := bufio.NewScanner(targetFile)
+		skip := false
+		for scanner.Scan() {
+			if strings.Contains(scanner.Text(), header) {
+				skip = true
 			}
 
-			if err := scanner.Err(); err != nil {
-				log.Fatal(err)
+			if strings.Contains(scanner.Text(), footer) {
+				skip = false
+				continue
 			}
 
-			if _, err := newContent.WriteString(cfg.Comment + " " + header + "\n"); err != nil {
-				log.Fatalln(err)
-			}
-			if _, err := newContent.WriteString(cfg.Append); err != nil {
-				log.Fatalln(err)
-			}
-			if _, err := newContent.WriteString("\n" + cfg.Comment + " " + footer + "\n"); err != nil {
-				log.Fatalln(err)
+			if skip {
+				continue
 			}
 
-			if err := os.WriteFile(cfg.Target, []byte(newContent.String()), os.ModePerm); err != nil {
-				log.Fatalf("failed writing target file (%v): %v\n", cfg.Target, err)
-			}
+			newContent.Write(scanner.Bytes())
+			newContent.WriteByte('\n')
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		if _, err := newContent.WriteString(cfg.Comment + " " + header + "\n"); err != nil {
+			log.Fatalln(err)
+		}
+		if _, err := newContent.WriteString(cfg.Append); err != nil {
+			log.Fatalln(err)
+		}
+		if _, err := newContent.WriteString("\n" + cfg.Comment + " " + footer + "\n"); err != nil {
+			log.Fatalln(err)
+		}
+
+		if err := os.WriteFile(cfg.Target, []byte(newContent.String()), os.ModePerm); err != nil {
+			log.Fatalf("failed writing target file (%v): %v\n", cfg.Target, err)
 		}
 	}
 }
