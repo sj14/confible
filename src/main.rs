6b271bc8b1ffeed80440303be5fc4fcfc28ee3dd
Simon Jürgensmeyer <simon.juergensmeyer@gmail.com>
2021-03-09 17:33:27 +0100

    Allow multiple config files

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,13 @@ use std::io::Seek;
 use std::io::Write;
 
 fn main() {
-    let config_file = std::env::args().nth(2).expect("no config file given");
+    let config_files : Vec<String> = std::env::args().skip(2).collect();
+    if config_files.len() == 0 {
+        println!("no config file given");
+        std::process::exit(1);
+    }
 
-    read_target_file(config_file);
+    read_target_file(config_files);
 }
 
 #[derive(Deserialize)]
@@ -22,64 +26,69 @@ struct Config {
     append: String,
 }
 
-fn read_target_file(config_file: String) {
-    let mut file = OpenOptions::new()
-        .write(true)
-        .read(true)
-        .open(config_file)
-        .expect("failed open");
-
-    let mut content = String::new();
-    file.read_to_string(&mut content).expect("failed reading");
-
-    let appender_configs: Configs =
-        toml::from_str(content.as_ref()).expect("faild reading toml file");
-
-    for cfg in appender_configs.configs {
-        println!("writing {}", cfg.target);
-
-        let boundary_start = "CONFIBLE START";
-        let boundary_stop = "CONFIBLE END";
-
-        let header = format!("\n{} {}\n", cfg.comment, boundary_start);
-        let footer = format!("\n{} {}\n", cfg.comment, boundary_stop);
+fn read_target_file(config_files: Vec<String>) {
+    for config_file in config_files {
         let mut file = OpenOptions::new()
             .write(true)
             .read(true)
-            .create(true)
-            .open(cfg.target)
+            .open(config_file)
             .expect("failed open");
 
         let mut content = String::new();
         file.read_to_string(&mut content).expect("failed reading");
 
-        // remove old appender config if exists
-        let mut new_content = String::new();
-        let mut should_add = true;
-        for l in content.lines() {
-            if l.contains(boundary_start) {
-                new_content.pop(); // remove empty new line
-                should_add = false;
-            }
-            if l.contains(boundary_stop) {
-                should_add = true;
-                continue;
+        let appender_configs: Configs =
+            toml::from_str(content.as_ref()).expect("faild reading toml file");
+
+        for cfg in appender_configs.configs {
+            println!("writing {}", cfg.target);
+
+            let boundary_start = "CONFIBLE START";
+            let boundary_stop = "CONFIBLE END";
+
+            let header = format!("\n{} {}\n", cfg.comment, boundary_start);
+            let footer = format!("\n{} {}\n", cfg.comment, boundary_stop);
+            let mut file = OpenOptions::new()
+                .write(true)
+                .read(true)
+                .create(true)
+                .open(cfg.target)
+                .expect("failed open");
+
+            let mut content = String::new();
+            file.read_to_string(&mut content).expect("failed reading");
+
+            // remove old appender config if exists
+            let mut new_content = String::new();
+            let mut should_add = true;
+            for l in content.lines() {
+                if l.contains(boundary_start) {
+                    new_content.pop(); // remove empty new line
+                    should_add = false;
+                }
+                if l.contains(boundary_stop) {
+                    should_add = true;
+                    continue;
+                }
+
+                if should_add {
+                    new_content.push_str(&format!("{}\n", l));
+                }
             }
 
-            if should_add {
-                new_content.push_str(&format!("{}\n", l));
-            }
+            // TODO: aggregate all configs for a target before writing.alloc
+            //       e.g. the same target could be used in multiple [[config]]
+
+            // overwrite file with new config
+            // TODO: add backup of file
+            file.seek(std::io::SeekFrom::Start(0))
+                .expect("failed seeking");
+            file.write_all(new_content.as_bytes())
+                .expect("failed writing content");
+            file.write_all(header.as_bytes()).expect("failed writing");
+            file.write_all(cfg.append.as_bytes())
+                .expect("failed writing");
+            file.write_all(footer.as_bytes()).expect("failed writing");
         }
-
-        // overwrite file with new config
-        // TODO: add backup of file
-        file.seek(std::io::SeekFrom::Start(0))
-            .expect("failed seeking");
-        file.write_all(new_content.as_bytes())
-            .expect("failed writing content");
-        file.write_all(header.as_bytes()).expect("failed writing");
-        file.write_all(cfg.append.as_bytes())
-            .expect("failed writing");
-        file.write_all(footer.as_bytes()).expect("failed writing");
     }
 }
